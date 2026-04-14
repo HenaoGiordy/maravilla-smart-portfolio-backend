977c0d116233f0538799191482fa3976e5129f35
HenaoGiordy <henaogiordy@gmail.com>
2026-04-13 22:01:22 -0500

    feat(cors): cors

--- app/main.py
+++ app/main.py
@@ -36,6 +36,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5174",
         "http://localhost:5174",
+        "https://${{ maravilla-smart-portfolio-frontend.RAILWAY_PUBLIC_DOMAIN }}"
     ],
     allow_credentials=True,
     allow_methods=["*"],
